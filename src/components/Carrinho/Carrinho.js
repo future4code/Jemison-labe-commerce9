a1a6bec49f9be1a80ca05a52ba91a6cf94a1e6e4
rafix923 <rafaellopes.dev@gmail.com>
2022-07-20 21:57:32 -0300

    Iniciar criação do carrinho de produtos

--- src/components/Carrinho/Carrinho.js
+++ src/components/Carrinho/Carrinho.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Botao, Cabecalho } from './style'
 
 function Carrinho() {
+
+  const title = "Carrinho de Produtos"
   return (
-    <div>Carrinho</div>
+    <div>
+      <Cabecalho>
+        <h1>{title}</h1>
+      </Cabecalho>
+    <Botao>Excluir produto</Botao>
+    </div>
   )
 }
 
