b3a3f56c804c110a7e5670b8373277be8e7436a7
Jefferson Vieira <myjeff22vieira@gmail.com>
2022-07-21 21:28:19 -0300

    mudancas-card

--- src/App.js
+++ src/App.js
@@ -1,15 +1,47 @@
-import React from 'react';
-
+import React,{useState}from 'react';
 import './App.css';
 import Carrinho from './components/Carrinho/Carrinho';
 import { Home } from './components/Home/Home';
+import { listaDeDados } from './components/Home/MockDeDados';
+
+
+
 function App() {
+
+
+
+  const {produtos}=listaDeDados;
+
+  const[camisas,setCamisas]=useState([]);
+
+  const adicionar=(produto)=>{
+
+   const existe = camisas.find(
+       (x)=>x.id===produto.id
+       )
+       if (existe){
+           setCamisas(
+              camisas.map((x)=>
+              x.id===produto.id?{...existe,qtd:existe.qtd+1}:x
+              ) 
+           )
+       } else {
+
+       }setCamisas([...setCamisas,{...produto,qtd:1}])
+  }
+
+
+
+
   return (
     <div className="App">
-     
+
       <Carrinho />
     </div>
   );
+
+
+
 }
 
 export default App;
