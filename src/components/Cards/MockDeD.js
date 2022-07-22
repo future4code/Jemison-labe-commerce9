13455f568a776e261973fc3554e34f2decea1297
karolina-azevedo <karolinam.azevedo@yahoo.com.br>
2022-07-21 22:15:31 -0300

    mudanças

---
+++ src/components/Cards/MockDeD.js
@@ -0,0 +1,33 @@
+export const listaProdutos = [
+  {
+     
+     title: "Camisa1",
+     price: 8000
+    
+  },
+  {
+   
+     title: "Camisa2",
+     price: 2000,
+
+  },
+  {
+     title: "Camisa3",
+     price: 50,
+    
+  },
+  {
+    
+     title: "Camisa4",
+     price: 80,
+    
+  },
+  {
+     title: "Camisa5",
+    price: 100
+  },
+  {
+     title: "Camisa6",
+     price: 100
+     
+  }]
---
+++ src/components/Cards/style.js
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+
+export const CardContainer = styled.div`
+   background-color: lavender;
+   margin: 5px;
+   padding: 0 15px;
+   border-radius: 5px;
+   width: 250px;
+`
+
+
+export const CardFooter = styled.h4`
+   display: flex;
+   justify-content: space-between;
+`
